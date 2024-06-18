e754488218cb8b57b92ee6f809dcc46e455160f8
tianmaxingkong12 <1498420415@qq.com>
2024-06-18 06:05:23 +0000

    Add Lesion load anno in COCO format

---
+++ dataset_utils/Lesion_COCO.py
@@ -0,0 +1,66 @@
+from pycocotools.coco import COCO
+import itertools
+
+def _isArrayLike(obj):
+    return hasattr(obj, '__iter__') and hasattr(obj, '__len__')
+
+class Lesion_COCO(COCO):
+
+    def getImgIds(self, imgIds=[], catIds=[], contain_difficult = True):
+        '''
+        Get img ids that satisfy given filter conditions.
+        :param imgIds (int array) : get imgs for given ids
+        :param catIds (int array) : get imgs with all given cats
+        :param contain_difficult (boolean) : whether contains difficult images 
+        :return: ids (int array)  : integer array of img ids
+        '''
+        imgIds = imgIds if _isArrayLike(imgIds) else [imgIds]
+        catIds = catIds if _isArrayLike(catIds) else [catIds]
+
+        if len(imgIds) == len(catIds) == 0:
+            ids = self.imgs.keys()
+        else:
+            ids = set(imgIds)
+            for i, catId in enumerate(catIds):
+                if i == 0 and len(ids) == 0:
+                    ids = set(self.catToImgs[catId])
+                else:
+                    ids &= set(self.catToImgs[catId])
+        print("***",contain_difficult)
+        if not contain_difficult:
+            ids = [_id for _id in ids if self.imgs[_id]["isdifficult"] == 0]
+        return list(ids)
+    
+    def getAnnIds(self, imgIds=[], catIds=[], areaRng=[], iscrowd=None, istypical=None):
+        """
+        Get ann ids that satisfy given filter conditions. default skips that filter
+        :param imgIds  (int array)     : get anns for given imgs
+               catIds  (int array)     : get anns for given cats
+               areaRng (float array)   : get anns for given area range (e.g. [0 inf])
+               iscrowd (boolean)       : get anns for given crowd label (False or True)
+               istypical (boolean)     : get anns for given typical label (False or True)
+        :return: ids (int array)       : integer array of ann ids
+        """
+        imgIds = imgIds if _isArrayLike(imgIds) else [imgIds]
+        catIds = catIds if _isArrayLike(catIds) else [catIds]
+
+        if len(imgIds) == len(catIds) == len(areaRng) == 0:
+            anns = self.dataset['annotations']
+        else:
+            if not len(imgIds) == 0:
+                lists = [self.imgToAnns[imgId] for imgId in imgIds if imgId in self.imgToAnns]
+                anns = list(itertools.chain.from_iterable(lists))
+            else:
+                anns = self.dataset['annotations']
+            anns = anns if len(catIds)  == 0 else [ann for ann in anns if ann['category_id'] in catIds]
+            anns = anns if len(areaRng) == 0 else [ann for ann in anns if ann['area'] > areaRng[0] and ann['area'] < areaRng[1]]
+        if not iscrowd == None:
+            ids = [ann['id'] for ann in anns if ann['iscrowd'] == iscrowd]
+        else:
+            ids = [ann['id'] for ann in anns]
+        if istypical:
+            ids = [ann['id'] for ann in anns if ann['istypical'] == istypical]
+        else:
+            ids = [ann['id'] for ann in anns]
+        return ids
+    
